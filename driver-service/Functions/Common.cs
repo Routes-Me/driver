a85d4f59b7cb7322c34c24d1536eee852ec340c6
aIjaz <aIjaz@routesme.com>
2022-01-26 17:13:34 +0500

    Remove Device Reference

--- driver-service/Functions/Common.cs
+++ driver-service/Functions/Common.cs
@@ -20,58 +20,5 @@ namespace driver_service.Functions
 
             return JArray.Parse(modelsJson);
         }
-
-        public IRestResponse PostAPI(string url, dynamic objectToSend)
-        {
-            var client = new RestClient(url);
-            var request = new RestRequest(Method.POST);
-            string jsonToSend = JsonConvert.SerializeObject(objectToSend);
-            request.AddParameter("application/json; charset=utf-8", jsonToSend, ParameterType.RequestBody);
-            request.RequestFormat = DataFormat.Json;
-            IRestResponse response = client.Execute(request);
-            if (response.StatusCode != HttpStatusCode.Created)
-                throw new Exception(response.Content);
-            return response;
-        }
-
-        public IRestResponse PutAPI(string url, dynamic objectToSend)
-        {
-            var client = new RestClient(url);
-            var request = new RestRequest(Method.PUT);
-            string jsonToSend = JsonConvert.SerializeObject(objectToSend);
-            request.AddParameter("application/json; charset=utf-8", jsonToSend, ParameterType.RequestBody);
-            request.RequestFormat = DataFormat.Json;
-            IRestResponse response = client.Execute(request);
-            if (response.StatusCode != HttpStatusCode.OK)
-                throw new Exception(response.Content);
-            return response;
-        }
-
-        public IRestResponse DeleteAPI(string url)
-        {
-            var client = new RestClient(url);
-            var request = new RestRequest(Method.DELETE);
-            return client.Execute(request);
-        }
-
-        public dynamic GetAPI(string url)
-        {
-            var client = new RestClient(url);
-            var request = new RestRequest(Method.GET);
-            IRestResponse response = client.Execute(request);
-
-            if (response.StatusCode != HttpStatusCode.OK)
-                throw new HttpListenerException(400);
-
-            return response;
-        }
-        public UriBuilder AppendQueryToUrl(UriBuilder baseUri, string queryToAppend)
-        {
-            if (baseUri.Query != null && baseUri.Query.Length > 1)
-                baseUri.Query = baseUri.Query.Substring(1) + "&" + queryToAppend;
-            else
-                baseUri.Query = queryToAppend;
-            return baseUri;
-        }
     }
 }
